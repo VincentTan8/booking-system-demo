5c91bbdf152dc834203d87329378ef5e4d7b91e5
VincentTan8 <vincent.tan@wetalk.com>
2025-03-20 11:39:31 +0800

    refactor: variable named tables

--- add-booking.php
+++ add-booking.php
@@ -7,13 +7,16 @@ if (!isset($_SESSION)) {
 
 <?php
 // Database connection
-include 'dbname.php';
+include 'conf.php';
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 $presentdate = date('Y-m-d H:i:s');
+$bookingtable = $prefix . "_resources.`booking`";
+$tistable = $prefix . "_resources.`teachers_in_sched`";
+$scheduletable = $prefix . "_resources.`schedule`";
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -22,24 +25,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $schedgroup_id = mysqli_real_escape_string($conn, $_POST['schedule']);
     $teacher_id = mysqli_real_escape_string($conn, $_POST['teacher']);
 
-    $result = $conn->query("SELECT `scheddate`, `schedstarttime` FROM `teachers_in_sched` WHERE `id` = $schedgroup_id");
+    $result = $conn->query("SELECT `scheddate`, `schedstarttime` FROM $tistable WHERE `id` = $schedgroup_id");
     $row = $result->fetch_assoc();   //no loop since we are expecting one row only
     $scheddate = $row["scheddate"];
     $schedstarttime = $row["schedstarttime"];
 
-    $result = $conn->query("SELECT `id` FROM `schedule` 
+    $result = $conn->query("SELECT `id` FROM $scheduletable 
         WHERE `scheddate` = '$scheddate' AND `schedstarttime` = '$schedstarttime' AND `teacher_id` = $teacher_id;"); //enclose date and time in single quotes
     $row = $result->fetch_assoc();
     $schedule_id = $row["id"];
 
     // SQL query to insert data
-    $sql = "INSERT INTO `booking` (`schedule_id`, `student_id`, `encoded_by`) 
+    $sql = "INSERT INTO $bookingtable (`schedule_id`, `student_id`, `encoded_by`) 
             VALUES ('$schedule_id', '$student_id', 'CS Person - $presentdate');";
 
     // Execute query and check for success
     if (mysqli_query($conn, $sql)) {
         $last_id = mysqli_insert_id($conn);
-        $conn->query("UPDATE `schedule` SET `booking_id` = $last_id WHERE `id` = $schedule_id");
+        $conn->query("UPDATE $scheduletable SET `booking_id` = $last_id WHERE `id` = $schedule_id");
         echo "Booking added successfully!<br>";
         echo "<a href='index.php'><button>Home</button></a>";
     } else {
